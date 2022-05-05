1cbd99dfe33343f811d969381ea9f64a6937dadd
tomcdev63 <tomcdev63@gmail.com>
2022-05-05 13:48:02 +0000

    sound ok

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 import time, os, re
 
-USB_TEST = "082b5a3d-dd9b-478e-8a7d-6c906466e6b3"
+USB_TEST = '082b5a3d-dd9b-478e-8a7d-6c906466e6b3'
+COMMANDE_SOUND = 'mpg123 "../data/viead.mp3"'
+COMMANDE_SOUND_OFF = "pkill mpg123"
 
+# Liste 
 devices = os.popen('sudo blkid').readlines()
 
 usbs = []
@@ -15,12 +18,18 @@ for u in devices:
     usbs.append(dict(zip(columns,loc)))
 
 print(usbs[0]["PARTUUID"])
+print("")
+print(usbs[1]["PARTUUID"])
+
+print(usbs[0]["PARTUUID"])
+print(type(usbs[0]["PARTUUID"]))
+print(type(USB_TEST))
 
 if usbs[0]["PARTUUID"] == USB_TEST:
 
-    commande_sound = 'mpg123 "../data/viead.mp3"'
-    commande_sound_closed = "pkill mpg123"
-
-    process = os.popen(commande_sound)
+    process = os.popen(COMMANDE_SOUND)
     time.sleep(10)
-    os.popen(commande_sound_closed)
+    os.popen(COMMANDE_SOUND_OFF)
+
+else:
+    print(usbs[0]["PARTUUID"], usbs[0]["PARTUUID"] == USB_TEST)
