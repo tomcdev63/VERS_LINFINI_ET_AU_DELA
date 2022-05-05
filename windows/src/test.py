942bc7ebbe8789c587014f5e7f51841dc5e12e4c
tomcdev63 <tomcdev63@gmail.com>
2022-05-05 23:58:01 +0200

    windows V2

--- windows/src/test.py
+++ windows/src/test.py
@@ -1,17 +1,30 @@
 import serial.tools.list_ports
-import time
 import playsound
+import time
+import os
 import requests
+import json
+
+
 
 while True:
   ports = serial.tools.list_ports.comports()
   
   if ports != []:  
-    playsound.playsound(r"G:\TAF\TOMDEV\VERS_LINFINI_ET_AU_DELA\windows\data\buzz.mp3")
-    time.sleep(3)
-    playsound.playsound(r"G:\TAF\TOMDEV\VERS_LINFINI_ET_AU_DELA\windows\data\welcome.mp3")
-
+    # playsound.playsound(r"G:\TAF\TOMDEV\VERS_LINFINI_ET_AU_DELA\windows\data\buzz.mp3")
+    # time.sleep(3)
+    # playsound.playsound(r"G:\TAF\TOMDEV\VERS_LINFINI_ET_AU_DELA\windows\data\welcome.mp3")
     code_postal = input("Quel est le code postal du lieu de votre obeservation? ")
     response = requests.get(f"https://api-adresse.data.gouv.fr/search/?q=postcode={code_postal}")
     resp_json_payload = response.json()
+
+
     print(resp_json_payload)
+
+
+  
+
+    
+    
+    break
+
