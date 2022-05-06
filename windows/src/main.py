94f7503693c5077c95576248e07eac7876e0816f
tomcdev63 <tomcdev63@gmail.com>
2022-05-06 23:02:26 +0200

    winok

--- windows/src/main.py
+++ windows/src/main.py
@@ -7,18 +7,22 @@ import webbrowser
 
 
 root = Tk()
-root.geometry("400x300") 
+root.geometry("450x250")
+root.iconbitmap('../data/buzz.ico')
 root.configure(bg='ghost white')
 root.title("VIEAD - CelestronC9.25 fastar")
+
+bg = PhotoImage(file="G:/TAF/TOMDEV/VERS_LINFINI_ET_AU_DELA/windows/data/wallpaper.png")
+my_label = Label(root, image=bg).pack()
 
 Label(root, text = "VERS L'INFINI ET AU DELA!", font = "arial 20 bold", bg='white smoke').pack()
 Label(text ="DataFlair", font = 'arial 15 bold', bg ='white smoke' , width = '20').pack(side = 'bottom')
 Msg = StringVar()
-Label(root,text ="Code Postal du lieu d'obeservation actuelle :", font = 'arial 11', bg ='white smoke').place(x=20,y=60)
+Label(root,text ="Code Postal du lieu d'obeservation actuelle :", font = 'arial 11 bold', bg ='white smoke').place(x=35,y=20)
 COORDONNEES = StringVar(value="Coords: ")
-label_coords = Label(root,textvariable =COORDONNEES, font = 'arial 15 bold', bg ='white smoke').place(x=50,y=200)
-entry_field = Entry(root, textvariable = Msg ,width ='50')
-entry_field.place(x=20,y=100)
+label_coords = Label(root,textvariable =COORDONNEES, font = 'arial 15 bold', bg ='white smoke').place(x=35,y=200)
+entry_field = Entry(root, textvariable = Msg ,width ='63')
+entry_field.place(x=35,y=60)
 
 celestron_connected = BooleanVar(value=False)
 def task():
@@ -52,22 +56,29 @@ def get_coordonees(code_postal):
     LATITUDE = resp_json_payload["features"][2]["geometry"]["coordinates"]
     return LATITUDE
 
-def get_astres_remarquables():
+def get_infos():
     webbrowser.open("https://www.stelvision.com/astro/a-voir-actuellement-dans-le-ciel/")
+    webbrowser.open("https://laclefdesetoiles.com/tubes-optiques-seuls/115-tube-optique-schmidt-cassegrain-celestron-c925-fastar-losmandy-c91027.html#:~:text=Le%20tube%20optique%20Celestron%20C9,ciel%20profond%20à%20grand%20champ.")
+    webbrowser.open("https://stellarium-web.org")
+    webbrowser.open("https://www.lameteoagricole.net")
 
 def Exit():
+    playsound.playsound(r"G:\TAF\TOMDEV\VERS_LINFINI_ET_AU_DELA\windows\data\goodbye.mp3")
     root.destroy()
+
 
 def Reset():
     Msg.set("")
+    COORDONNEES.set("")
 
 def Welcome():
     playsound.playsound(r"G:\TAF\TOMDEV\VERS_LINFINI_ET_AU_DELA\windows\data\welcome.mp3")
 
-Button(root, text = "PLAY", font = 'arial 15 bold' , command = Text_to_speech ,width = '4').place(x=25,y=140)
-Button(root, font = 'arial 15 bold',text = 'EXIT', width = '4' , command = Exit, bg = 'OrangeRed1').place(x=100 , y = 140)
-Button(root, font = 'arial 15 bold',text = 'RESET', width = '6' , command = Reset).place(x=175 , y = 140)
-Button(root, font = 'arial 15 bold',text = 'INFOS', width = '6' , command = get_astres_remarquables).place(x=275 , y = 140)
+Button(root, text = "PLAY", font = 'arial 15 bold' , command = Text_to_speech ,width = '6', bg = 'Green').place(x=35,y=98)
+Button(root, font = 'arial 15 bold',text = 'EXIT', width = '6' , command = Exit, bg = 'Red').place(x=135 , y = 98)
+Button(root, font = 'arial 15 bold',text = 'RESET', width = '6' , command = Reset, bg = 'Orange').place(x=235 , y = 98)
+Button(root, font = 'arial 15 bold',text = 'INFOS', width = '6' , command = get_infos).place(x=335 , y = 98)
 
+Welcome()
 root.after(500, task)
 root.mainloop()
